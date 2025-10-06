b27f330135cdc40a95940efdc16f525ebe7c6e4f
Yashraaj Sharad Gorde <149304566+yashraj-gorde@users.noreply.github.com>
2025-10-06 09:11:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
 //your code here!
 
+const infiList = document.getElementById("infi-list");
+
+function addListItems(count) {
+  const currentCount = infiList.children.length;
+  for (let i = 1; i <= count; i++) {
+    const li = document.createElement("li");
+    li.textContent = `Item ${currentCount + i}`;
+    infiList.appendChild(li);
+  }
+}
+
+addListItems(10);
+
+
+infiList.addEventListener("scroll", () => {
+  if (infiList.scrollTop + infiList.clientHeight >= infiList.scrollHeight) {
+    addListItems(2); 
+  }
+});
